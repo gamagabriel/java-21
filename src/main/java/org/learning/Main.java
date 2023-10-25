71dab9027e9e224e1a46fbe9876eeba1bd8d4406
gamagabriel <gbr.gama99@gmail.com>
2023-10-25 00:36:42 -0300

    Initial Commit

---
+++ src/main/java/org/learning/Main.java
@@ -0,0 +1,7 @@
+package org.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This project was created to learning about Java 21.");
+    }
+}
